8e3c4b26d90ee186ac3fce98139da200cc5e7369
Harjyot Bagga <imharjyotbagga@gmail.com>
2020-04-12 23:59:48 +0530

    DB ready

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    phone = models.BigIntegerField(null=False)
+    email = models.EmailField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    price = models.FloatField(null=False)
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+    category = models.CharField(max_length=50, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', "Out for Delivery"),
+        ('Delivered', 'Delivered')
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL, related_name='customer_orders')
+    product = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL, related_name='product_orders')
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS, null=False, max_length=50)
+    
+    def __str__(self):
+        return self.date_created
